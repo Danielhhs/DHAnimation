9dcea9860f21425153a8ef337ae442df30269a83
hhuang <hhuang@microstrategy.com>
2016-03-16 09:12:52 +0800

    Add a little stop to Grid animation;

--- DHAnimation/Animations/Grid/GridDestinationVertex.glsl
+++ DHAnimation/Animations/Grid/GridDestinationVertex.glsl
@@ -10,24 +10,31 @@ layout(location = 2) in vec2 a_texCoords;
 
 out vec2 v_texCoords;
 
-const float zoomInRatio = 0.15;
-const float zoomOutRatio = 0.15;
-const float transitionRatio = 0.7;
+const float zoomInRatio = 0.2;
+const float zoomInStopRatio = 0.1;
+const float zoomOutRatio = 0.2;
+const float zoomOutStopRatio = 0.1;
+const float transitionRatio = 0.4;
 
 vec4 updatedPosition() {
     vec4 position = a_position;
     
     if (u_percent <= zoomOutRatio) {
         position.x += 1.5 * u_screenWidth;
-    } else if (u_percent <= zoomOutRatio + transitionRatio) {
-        float offset = (1.f - (u_percent - zoomOutRatio) / transitionRatio) * 1.5 * u_screenWidth;
+    } else if (u_percent <= zoomOutRatio + zoomOutStopRatio) {
+        position.x += 1.5 * u_screenWidth;
+        position.z = -500.f;
+    } else if (u_percent <= zoomOutRatio + zoomOutStopRatio + transitionRatio) {
+        float offset = (1.f - (u_percent - zoomOutRatio - zoomOutStopRatio) / transitionRatio) * 1.5 * u_screenWidth;
         if (u_direction == 1) {
             offset *= -1.f;
         }
         position.x += offset;
-        position.z = -300.f;
+        position.z = -500.f;
+    } else if (u_percent <= zoomOutStopRatio + zoomOutRatio + transitionRatio + zoomInStopRatio) {
+        position.z = -500.f;
     } else {
-        position.z = (1.f - (u_percent - zoomOutRatio - transitionRatio) / zoomInRatio) * -300.f;
+        position.z = (1.f - (u_percent - zoomOutRatio - transitionRatio - zoomInStopRatio - zoomOutStopRatio) / zoomInRatio) * -500.f;
     }
     return position;
 }
--- DHAnimation/Animations/Grid/GridSourceVertex.glsl
+++ DHAnimation/Animations/Grid/GridSourceVertex.glsl
@@ -10,18 +10,22 @@ layout(location = 2) in vec2 a_texCoords;
 
 out vec2 v_texCoords;
 
-const float zoomInRatio = 0.15;
-const float zoomOutRatio = 0.15;
-const float transitionRatio = 0.7;
+const float zoomInRatio = 0.2;
+const float zoomInStopRatio = 0.1;
+const float zoomOutRatio = 0.2;
+const float zoomOutStopRatio = 0.1;
+const float transitionRatio = 0.4;
 
 vec4 updatedPosition()
 {
     vec4 position = a_position;
     if (u_percent <= zoomOutRatio) {
-        position.z = -1.f * u_percent / zoomInRatio * 300.f;
-    } else if (u_percent <= zoomOutRatio + transitionRatio) {
-        position.z = -300.f;
-        float offset = (u_percent - zoomOutRatio) / transitionRatio * u_screenWidth * 1.5;
+        position.z = -1.f * u_percent / zoomInRatio * 500.f;
+    } else if (u_percent <= zoomOutRatio + zoomOutStopRatio){
+        position.z = -500.f;
+    } else if (u_percent <= zoomOutRatio + zoomOutStopRatio + transitionRatio) {
+        position.z = -500.f;
+        float offset = (u_percent - zoomOutRatio - zoomOutStopRatio) / transitionRatio * u_screenWidth * 1.5;
         if (u_direction == 1) {
             offset *= -1.f;
         }
