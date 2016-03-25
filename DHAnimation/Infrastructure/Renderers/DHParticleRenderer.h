ed6ce3b1a261ba430e6a75078196f616cfbd2522
hhuang <hhuang@microstrategy.com>
2016-03-25 14:15:03 +0800

    Reorganise structure;

---
+++ DHAnimation/Infrastructure/Renderers/DHParticleRenderer.h
@@ -0,0 +1,13 @@
+//
+//  DHParticleRenderer.h
+//  DHAnimation
+//
+//  Created by Huang Hongsen on 3/25/16.
+//  Copyright © 2016 cn.daniel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DHParticleRenderer : NSObject
+
+@end
